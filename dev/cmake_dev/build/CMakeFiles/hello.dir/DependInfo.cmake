2873d562b54d72896dc4810ccc56c83db627bfc6
Morris Huang <morris8934@icloud.com>
2021-09-01 17:01:14 +0800

    use shell script for building hdf5 and md program independetely

---
+++ dev/cmake_dev/build/CMakeFiles/hello.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/work/morris123/MD/dev/cmake_dev/src/hello.f90" "/work/morris123/MD/dev/cmake_dev/build/CMakeFiles/hello.dir/src/hello.f90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "Intel")
+set(CMAKE_Fortran_SUBMODULE_SEP "@")
+set(CMAKE_Fortran_SUBMODULE_EXT ".smod")
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
